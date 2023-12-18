d048c730467322a8180b8fbe52f4e55659097835
Nusab Taha <nusabtaha33@gmail.com>
2023-12-19 03:14:40 +0600

    force push

---
+++ public/main.py
@@ -0,0 +1,103 @@
+import os
+import json
+
+
+def makeDir(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+
+def makeJson(path, content):
+    with open(path, 'w', encoding="utf8") as f:
+        json.dump(content, f, ensure_ascii=0, indent=4)
+
+
+with open("Data/surah.json", 'r', encoding="utf8") as f:
+    surahNames = json.load(f)
+
+with open("Data/surahData.json", 'r', encoding="utf8") as f:
+    surahData = json.load(f)
+
+
+with open("Data/quran_ar.json", 'r', encoding="utf8") as f:
+    quranAr = json.load(f)
+
+with open("Data/quran_en.json", 'r', encoding="utf8") as f:
+    quranEn = json.load(f)
+
+
+makeDir("api")
+
+allSurahData = []
+for surahNo, j in quranEn.items():
+
+    audioUrl = "https://quranaudio.pages.dev/{num}/{surahNo}_{ayahNo}.mp3"
+    surahNo = int(surahNo)
+    totalAyah = len(j)
+    # surahName = surahNames[surahNo - 1]
+
+    surahInfo = surahData[surahNo - 1]
+
+    surahName = surahInfo["surahName"]
+    surahNameAr = surahInfo["surahNameAr"]
+    surahNameArLong = surahInfo["surahNameArLong"]
+    surahNameTranslation = surahInfo["surahNameMeaning"]
+    revelationPlace = surahInfo["revelationPlace"]
+
+    ara = quranAr[str(surahNo)]
+    makeDir(f"api/{surahNo}")
+
+    for ayahNo, english in enumerate(j, start=1):
+        
+        arabic1, arabic2 = ara[ayahNo - 1]
+        ayahData = {
+            "surahName": surahName,
+            "surahNameArabic": surahNameAr,
+            "surahNameArabicLong": surahNameArLong,
+            "surahNameTranslation": surahNameTranslation,
+            "revelationPlace": revelationPlace,
+            "totalAyah": totalAyah,
+            "surahNo": surahNo,
+            "ayahNo": ayahNo,
+            "english": english,
+            "arabic1": arabic1,
+            "arabic2": arabic2,
+            "audio": {
+                "1": {
+                    "reciter": "Mishary Rashid Al-Afasy",
+                    "url": audioUrl.format(num=1, surahNo=surahNo, ayahNo=ayahNo)
+                },
+                "2": {
+                    "reciter": "Abu Bakr Al-Shatri",
+                    "url": audioUrl.format(num=2, surahNo=surahNo, ayahNo=ayahNo)
+                }
+            }
+        }
+        makeJson(f"api/{surahNo}/{ayahNo}.json", ayahData)
+
+        print(f"Done {ayahNo} of {surahNo}\r", end='')
+    
+    finalData = {
+        "surahName": surahName,
+        "surahNameArabic": surahNameAr,
+        "surahNameArabicLong": surahNameArLong,
+        "surahNameTranslation": surahNameTranslation,
+        "revelationPlace": revelationPlace,
+        "totalAyah": totalAyah,
+        "surahNo": surahNo,
+        "english":j,
+        "arabic1": [i[0] for i in ara],
+        "arabic2": [i[1] for i in ara]
+    }
+    makeJson(f"api/{surahNo}.json", finalData)
+    allSurahData.append({
+        "surahName": surahName,
+        "surahNameArabic": surahNameAr,
+        "surahNameArabicLong": surahNameArLong,
+        "surahNameTranslation": surahNameTranslation,
+        "revelationPlace": revelationPlace,
+        "totalAyah": totalAyah,
+    })
+    print()
+
+makeJson("api/surah.json", allSurahData)
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { DocsThemeConfig } from "nextra-theme-docs"
+
+const config: DocsThemeConfig = {
+  logo: <span>Contest Hive</span>,
+  project: {
+    link: "https://github.com/Contest-Hive/Contest-Hive",
+  },
+  chat: {
+    link: "https://discordid.netlify.app/?id=804035616420397086",
+  },
+  docsRepositoryBase: "https://github.com/Contest-Hive/docs/tree/main/",
+  footer: {
+    text: "© Contest Hive 2023 - All rights reserved.",
+  },
+}
+
+export default config

eea9e1dd1a9ca7a4c7361d7fdfe948d7bbce3076
Nusab Taha <nusabtaha33@gmail.com>
2024-03-24 16:24:21 +0600

    fix project link and update about section

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,16 @@
-import React from "react"
 import { DocsThemeConfig } from "nextra-theme-docs"
 
 const config: DocsThemeConfig = {
-  logo: <span>Quran API</span>,
+  logo: <b>Quran API</b>,
   project: {
     link: "https://github.com/Nusab19/Quran-API",
   },
   chat: {
     link: "https://discordid.netlify.app/?id=804035616420397086",
   },
-  docsRepositoryBase: "https://github.com/Quran-API/docs/tree/main/",
+  docsRepositoryBase: "https://github.com/Nusab19/Quran-API/tree/main/",
   footer: {
-    text: "© Quran API 2023 - All rights reserved.",
+    text: "© Quran API 2023 - 2024. All rights reserved.",
   },
 }
 

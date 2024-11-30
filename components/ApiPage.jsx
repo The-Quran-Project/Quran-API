c0f24ad6059660d9923145b51b97c17d396e962c
Nusab Taha <nusabtaha33@gmail.com>
2024-11-30 17:09:51 +0600

    relative url instead of absolute url

---
+++ components/ApiPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ApiPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold my-5">Api Route</h1>
+      <p>
+        Visit{" "}
+        <a className="text-blue-500" href="/getting-started">
+          getting started
+        </a>{" "}
+        page to see how to use the api.
+      </p>
+    </div>
+  );
+};
+
+export default ApiPage;
---
+++ components/NotFound.jsx
@@ -0,0 +1,26 @@
+import { usePathname } from "next/navigation";
+import { NotFoundPage } from "nextra-theme-docs";
+import ApiPage from "./ApiPage";
+
+const NotFound = () => {
+  const pathname = usePathname();
+  if (pathname == "/api") {
+    return <ApiPage />;
+  }
+  return (
+    <div suppressHydrationWarning>
+      <h1 className="text-4xl font-bold my-5">Path not found</h1>
+      This page does not exist. Please check the URL or go back to the{" "}
+      <a href="/" className="text-blue-500">
+        homepage
+      </a>
+      .
+      <br />
+      <br />
+      <br />
+      <NotFoundPage />
+    </div>
+  );
+};
+
+export default NotFound;

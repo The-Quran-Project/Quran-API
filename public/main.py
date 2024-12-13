bc3a3c0271581187f5e1479c9c3d67ea64f29c36
Nusab Taha <nusabtaha33@gmail.com>
2024-12-13 21:13:02 +0600

    audio recitation of whole chapter

--- public/main.py
+++ public/main.py
@@ -2,13 +2,6 @@ import os
 import json
 
 
-# TODO: Add other reciters
-reciters = {
-    "1": "Mishary Rashid Al-Afasy",
-    "2": "Abu Bakr Al-Shatri",
-    "3": "Nasser Al Qatami",
-}
-
 def makeDir(folder_name):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
@@ -19,54 +12,61 @@ def makeJson(path, content):
         json.dump(content, file, ensure_ascii=0, indent=4)
 
 
+def readJsonFile(path: str):
+    with open(f"Data/{path}", "r", encoding="utf8") as file:
+        return json.load(file)
+
+
 if os.path.exists("public"):
     os.chdir("public")
 
-
-with open("Data/surah.json", "r", encoding="utf8") as f:
-    surahNames = json.load(f)
-
-with open("Data/surahData.json", "r", encoding="utf8") as f:
-    surahData = json.load(f)
-
-
-with open("Data/quran_ar.json", "r", encoding="utf8") as f:
-    quranAr = json.load(f)
-
-with open("Data/quran_en.json", "r", encoding="utf8") as f:
-    quranEn = json.load(f)
-
-with open("Data/quran_bn.json", "r", encoding="utf8") as f:
-    quranBn = json.load(f)
-
+reciters = {
+    "1": "Mishary Rashid Al-Afasy",
+    "2": "Abu Bakr Al-Shatri",
+    "3": "Nasser Al Qatami",
+}
+originalUrl = {
+    "1": "https://server8.mp3quran.net/download/afs/{}.mp3",
+    "2": "https://server11.mp3quran.net/download/shatri/{}.mp3",
+    "3": "https://server6.mp3quran.net/download/qtm/{}.mp3",
+}
 
 makeDir("api")
+makeDir("api/audio")
+makeJson("api/reciters.json", reciters)
+
+surahNames = readJsonFile("surah.json")
+surahData = readJsonFile("surahData.json")
+quranAr = readJsonFile("quran_ar.json")
+quranEn = readJsonFile("quran_en.json")
+quranBn = readJsonFile("quran_bn.json")
+
 
 allSurahData = []
-for surahNo, j in quranEn.items():
 
-    audioUrl = "https://quranaudio.pages.dev/{num}/{surahNo}_{ayahNo}.mp3"
-    surahNo = int(surahNo)
-    totalAyah = len(j)
-    # surahName = surahNames[surahNo - 1]
 
+for surahNo in range(1, 115):
+    eng = quranEn[surahNo - 1]
+    ara = quranAr[surahNo - 1]
+    ben = quranBn[surahNo - 1]
+
+    totalAyah = len(eng)
     surahInfo = surahData[surahNo - 1]
-
     surahName = surahInfo["surahName"]
     surahNameAr = surahInfo["surahNameAr"]
     surahNameArLong = surahInfo["surahNameArLong"]
     surahNameTranslation = surahInfo["surahNameMeaning"]
     revelationPlace = surahInfo["revelationPlace"]
-
-    ara = quranAr[str(surahNo)]
-    ben = quranBn[surahNo - 1]
+    verseAudio = "https://quranaudio.pages.dev/{num}/{surahNo}_{ayahNo}.mp3"
+    chapterAudio = "https://github.com/The-Quran-Project/Quran-Audio/raw/refs/heads/main/Data/{}/{}.mp3"
 
     # Make the folder
     makeDir(f"api/{surahNo}")
 
-    for ayahNo, english in enumerate(j, start=1):
+    for ayahNo in range(1, totalAyah + 1):
+        english = eng[ayahNo - 1]
         arabic1, arabic2 = ara[ayahNo - 1]
-        bangla = ben[ayahNo - 1]
+        bengali = ben[ayahNo - 1]
 
         ayahData = {
             "surahName": surahName,
@@ -80,24 +80,16 @@ for surahNo, j in quranEn.items():
             "english": english,
             "arabic1": arabic1,
             "arabic2": arabic2,
-            "bangla": bangla,
+            "bengali": bengali,
             "audio": {
-                "1": {
-                    "reciter": "Mishary Rashid Al-Afasy",
-                    "url": audioUrl.format(num=1, surahNo=surahNo, ayahNo=ayahNo),
-                },
-                "2": {
-                    "reciter": "Abu Bakr Al-Shatri",
-                    "url": audioUrl.format(num=2, surahNo=surahNo, ayahNo=ayahNo),
-                },
-                "3":{
-                   "reciter": reciters["3"],
-                "url":audioUrl.format(num=3, surahNo=surahNo, ayahNo=ayahNo)
-               
-               }
+                i: {
+                    "reciter": j,
+                    "url": verseAudio.format(num=i, surahNo=surahNo, ayahNo=ayahNo),
+                }
+                for (i, j) in reciters.items()
             },
         }
-        
+
         makeJson(f"api/{surahNo}/{ayahNo}.json", ayahData)
 
         print(f"Done {ayahNo} of {surahNo}\r", end="")
@@ -110,11 +102,30 @@ for surahNo, j in quranEn.items():
         "revelationPlace": revelationPlace,
         "totalAyah": totalAyah,
         "surahNo": surahNo,
-        "english": j,
+        "audio": {
+            i: {
+                "reciter": j,
+                "url": chapterAudio.format(i, surahNo),
+                "originalUrl": originalUrl[i].format(f"{surahNo:03}"),
+            }
+            for (i, j) in reciters.items()
+        },
+        "english": [i for i in eng],
         "arabic1": [i[0] for i in ara],
         "arabic2": [i[1] for i in ara],
+        "bengali": [i for i in ben],
+    }
+
+    chapterAudioData = {
+        i: {
+            "reciter": j,
+            "url": chapterAudio.format(i, surahNo),
+            "originalUrl": originalUrl[i].format(f"{surahNo:03}"),
+        }
+        for (i, j) in reciters.items()
     }
     makeJson(f"api/{surahNo}.json", finalData)
+    makeJson(f"api/audio/{surahNo}.json", chapterAudioData)
     allSurahData.append(
         {
             "surahName": surahName,
@@ -128,8 +139,3 @@ for surahNo, j in quranEn.items():
     print()
 
 makeJson("api/surah.json", allSurahData)
-
-
-
-makeJson("api/reciters.json", reciters)
-print("Done")
